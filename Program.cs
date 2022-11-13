f5dfa98eb4c930e5702295033422ff3902125d99
Julia Debelova <u.deb@mail.ru>
2022-11-13 21:51:23 +0500

    write code of program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetNewStringArrayWithShortItems(string[] array, int maxLength = 3)
+{
+    int length = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength)
+        {
+            length++;
+        }
+    }
+    string[] result = new string[length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength)
+        {
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+string OutputStringArray(string[] array)
+{
+    return array.Length == 0 ? "[]" : "[\"" + string.Join("\", \"", array) + "\"]";
+}
+
+
+string[] example1 = { "hello", "2", "world", ":-)" };
+string[] example2 = { "1234", "1567", "-2", "computer science" };
+string[] example3 = { "Russia", "Denmark", "Kazan" };
+
+string[] result1 = GetNewStringArrayWithShortItems(example1);
+string[] result2 = GetNewStringArrayWithShortItems(example2);
+string[] result3 = GetNewStringArrayWithShortItems(example3);
+
+Console.WriteLine(OutputStringArray(example1) + " -> " + OutputStringArray(result1));
+Console.WriteLine(OutputStringArray(example2) + " -> " + OutputStringArray(result2));
+Console.WriteLine(OutputStringArray(example3) + " -> " + OutputStringArray(result3));
